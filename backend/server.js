fa576b6064855c97c5d93141b08194880e12167d
Darshan <darshanrajpurohit47@gmail.com>
2022-08-15 15:41:08 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,10 @@ process.on("uncaughtException",err=>{
 
 
 //config
-dotenv.config({path:"backend/config/config.env"});
 
+if(process.env.NODE_ENV !== "PRODUCTION"){
+   require("dotenv").config({path:"backend/config/config.env"});
+}
 //connecting to database
 connectDatabase();
 cloudinary.config({
